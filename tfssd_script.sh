9f64fd97b7559e43b76e169eb48348483124d19a
Trine Aalborg <vm82rp@bio.aau.dk>
2022-10-10 11:59:15 +0000

    Created template for bash script

---
+++ tfssd_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Script for generating distribution plot of unique ID counts from 
+combined python and R scripts 
+#Auhtor Trine Aalborg
+#Version 2022-10-10
+#
+#Dependencies: supplied python (print_kpthesaurus.py) and R 
+(analyse_and_visualize.r) scripts from git repo: 
+https://github.com/aau-claaudia/sci-sw-dev
+#
+
+
+#
+echo "Your files have been processed and results printed in filename 
+datafile"
+
+
+#
+echo "Your filename data have been plotted"
+echo "Thank you for using the script for creating a distribution plot of 
+unique ID counts"
+
