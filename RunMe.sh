daa798dbba197ac0dd12bd1d8064104314583981
Sebastian <by07eg@et.aau.dk>
2022-10-12 12:53:37 +0200

    Modified RunMe to run the python script on all data files and append the output in the required format for the Rscript

--- RunMe.sh
+++ RunMe.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-touch Datasorting.dat   #Create text file for datasorting the ID's 
-> Datasorting.dat 	#Clear text file
+touch Datasorting.dat 							#Create text file for datasorting the ID's 
+> Datasorting.dat 							#Clear text file
 
-casename="001-100865;"
-IDoutput=$((python print_kpthesaurus.py data/001-100865.json) 2>&1)
-echo "$casename$IDoutput" >> Datasorting.dat
+for casefile in data/*.json						#Run python script on all datafiles in directory data
+do
+	output=$((python print_kpthesaurus.py "$casefile") 2>&1)
+        filename="${casefile##*/}"; filename="${filename%.*}"		#Combine filename and output from python script into Rscript format
+        echo "$filename;$output" >> Datasorting.dat			#Append combined name in Datasorting file
+done
 
-casename1="001-100864;"
-IDoutput1=$((python print_kpthesaurus.py data/001-100864.json) 2>&1)
-echo "$casename1$IDoutput1" >> Datasorting.dat
-
-Rscript analyse_and_visualize.r Datasorting.dat
+Rscript analyse_and_visualize.r Datasorting.dat				#Run Rscript to produce plot of the sorted data
 
