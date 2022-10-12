67a1afc1acab2277086e616d90ef445b96263f20
Sebastian <by07eg@et.aau.dk>
2022-10-12 10:35:43 +0200

    Modified RunMe to add casenumber and IDoutput from pythonscript on two cases into Datasorting file

--- RunMe.sh
+++ RunMe.sh
@@ -3,4 +3,13 @@
 touch Datasorting.txt   #Create text file for datasorting the ID's 
 > Datasorting.txt	#Clear text file
 
-python print_kpthesaurus.py data/001-100865.json
+casename="001-100865;"
+IDoutput=$((python print_kpthesaurus.py data/001-100865.json) 2>&1)
+echo "$casename$IDoutput" >> Datasorting.txt
+
+casename1="001-100864;"
+IDoutput1=$((python print_kpthesaurus.py data/001-100864.json) 2>&1)
+echo "$casename1$IDoutput1" >> Datasorting.txt
+
+
+
