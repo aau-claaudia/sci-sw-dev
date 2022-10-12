18449120c287b14ead8b8d6b70dc3fd33456c91a
Sebastian <by07eg@et.aau.dk>
2022-10-12 10:43:53 +0200

    Modified RunMe to run Rscript on the created datasorting file

--- RunMe.sh
+++ RunMe.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-touch Datasorting.txt   #Create text file for datasorting the ID's 
-> Datasorting.txt	#Clear text file
+touch Datasorting.dat   #Create text file for datasorting the ID's 
+> Datasorting.dat 	#Clear text file
 
 casename="001-100865;"
 IDoutput=$((python print_kpthesaurus.py data/001-100865.json) 2>&1)
-echo "$casename$IDoutput" >> Datasorting.txt
+echo "$casename$IDoutput" >> Datasorting.dat
 
 casename1="001-100864;"
 IDoutput1=$((python print_kpthesaurus.py data/001-100864.json) 2>&1)
-echo "$casename1$IDoutput1" >> Datasorting.txt
+echo "$casename1$IDoutput1" >> Datasorting.dat
 
-
+Rscript analyse_and_visualize.r Datasorting.dat
 
