8390d5fc5238fdd4c31722f9d80ba70edb49919d
Ana Sofia Santos Cardoso <xv20xo@hst.aau.dk>
2022-10-17 15:48:28 +0200

    Move output directory (out) to src/out.
    The R script has trouble naming the output file when the input is
    in a directory outside src. Changing the structure minimized this
    problem.

--- src/clear_outputs.sh
+++ src/clear_outputs.sh
@@ -1,6 +1,6 @@
 # Remove files in subdirectories of /out
 
-for entry in "./out/dat"/* "./out/pdf"/*
+for entry in "out/dat"/* "out/pdf"/*
 do
   if [ -f "$entry" ];then
     rm "$entry"
--- src/extract_ID.sh
+++ src/extract_ID.sh
@@ -1,29 +1,33 @@
 
-while getopts f: flag
+# use flag -f to set a filename for the outputs
+while getopts f: flag 
 do
     case "${flag}" in
         f) filename=${OPTARG};;
     esac
 done
 
-if [[ -z $filename ]]; then
-    filename=id-list.dat
+if [[ -z $filename ]]; then # if no filename is set
+    filename=kpthesaurus    # use "kpthesaurus"
 fi
 
-touch ./out/dat/$filename.dat
-> ./out/dat/$filename.dat
+touch ./out/dat/$filename.dat   # touch new file
+> ./out/dat/$filename.dat       # clear file
 
-for entry in ./data/*.json
+for entry in ../data/*.json
     do  
         # get filename and remove extension
         casefile="${entry##*/}"; casefile="${casefile%.*}" 
 
         # run python script
-        output=$((python3 ./src/scripts/print_kpthesaurus.py "$entry") 2>&1)
+        output=$((python3 scripts/print_kpthesaurus.py "$entry") 2>&1)
 
- 
-        echo "$casefile;$output" >> ./out/dat/$filename.dat
+        # combine this case's filename name with the extracted IDs
+        echo "$casefile;$output" >> out/dat/$filename.dat
     done
 
-Rscript ./src/scripts/analyse_and_visualize.r ./out/dat/$filename.dat
-mv .pdf ./out/pdf/$filename.pdf
+# run the R script to produce the plot
+Rscript scripts/analyse_and_visualize.r out/dat/$filename.dat
+
+# move the pdf file to the appropriate directory
+mv out/dat/$filename.pdf out/pdf/$filename.pdf
