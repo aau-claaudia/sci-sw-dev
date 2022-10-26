464f22bece4505fc5a12f3834239a36b69e33ce6
Yubo <yubow@es.aau.dk>
2022-10-26 17:03:28 +0200

    update README.md

--- scripts.sh
+++ scripts.sh
@@ -2,17 +2,17 @@
 # Script for extract the IDs of each case and produce a plot of the distribution of different IDs in a sorted form
 # Author: Yubo Wang
 # Time: 2022-10-25
-# Version 1
+# Version 1.1
 
 rm -rf result.dat #delete original .dat file
 
 touch result.dat #create .dat file
 
-for files in data/*.json   # use python to extract the IDs
+for files in data/*.json   # loops through the json files in the /data folder
 do
 	IDs=$(python print_kpthesaurus.py $file) # use .py to extract the IDs
 	name=$(basename -s .json $file)  # use basename to extract file name
-	echo "$name;$IDs" >> result.dat  #combine name and IDs in .dat
+	echo "$name;$IDs" >> result.dat  #Use semicolons to separate names and IDs, and merge them in .dat.
 done
 
-Rscript analyse_and_visualize.r result.dat
+Rscript analyse_and_visualize.r result.dat # produce a plot
