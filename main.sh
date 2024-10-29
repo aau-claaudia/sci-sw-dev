937e9865b898a75aa278bd3a24bef61605d5d4a3
Signe Bjerregaard-Michelsen <signe.bm@gvdnet.dk>
2024-10-29 15:27:29 +0100

    created bash file and added a description

---
+++ main.sh
@@ -0,0 +1,5 @@
+# This bash script extracts the IDs of each case in the European Court of Human Rights and print the IDs to standard output, 
+# to produce a plot of the distribution/count of different IDs in a sorted form. This is done by first calling the Python 
+# script (print_kpthesaurus.py) and then the R script (analyse_and_visualize.r).
+
+
