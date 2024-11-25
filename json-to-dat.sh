93e4cf1e57524ab1e519623b7b98fe2636a3e173
Nikolaj Bak <ndbak16@gmail.com>
2024-11-25 13:46:03 +0100

    added explanations in json-to-dat.sh

--- json-to-dat.sh
+++ json-to-dat.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# make directory of python output
 mkdir -p python_output
 
+# change json files to dat files using python script
 for i in data/*.json; do
-	echo "input file $i"
+	echo "input file $i to python"
 	filename=$(basename "$i" .json)
     	python3 print_kpthesaurus.py "$i" > "python_output/${filename}.dat"
 done
 
-echo "Changed json to dat files."
 
+echo "Changed json to dat files."
