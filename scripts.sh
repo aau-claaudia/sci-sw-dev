70f0ecf816463e1daa39924d7e60730ea8730818
apvenergy <apv@energy.aau.dk>
2024-11-28 10:29:15 +0100

    Added the scripts file to automate

---
+++ scripts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script for extract the IDs of each case and produce a plot of the distribution of different IDs in a sorted form
+# Author: Arun P Vasudev
+# Time: 25 Oct 2024 
+# Version 1.1
+
+rm -rf result.dat #delete original .dat file
+
+touch result.dat #create .dat file
+
+for file in data/*.json   # loops through the json files in the /data folder
+do
+	IDs=$(python print_kpthesaurus.py $file) # use .py to extract the IDs
+	name=$(basename -s .json $file)  # use basename to extract file name
+	echo "$name;$IDs" >> result.dat  #Use semicolons to separate names and IDs, and merge them in .dat.
+done
+
+Rscript analyse_and_visualize.r result.dat # produce a plot
